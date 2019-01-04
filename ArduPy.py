ee77ddb1ac81e346eaa7822860aa3554c2c247aa
juxi34 <juxi34@localhost.localdomain>
2019-01-03 23:13:48 -0600

    Agregar variables ya sirve

--- ArduPy.py
+++ ArduPy.py
@@ -1,17 +1,53 @@
+import os
+
 nombre = str(input("Como se va a llamar tu programa: "))
 
-
 f = open("{}.ino".format(nombre), 'w')
-f.write("""
+
+variables = int(input("Cuantas varables deseas generar? "))
+
+for var in range (variables):
+	print("""
+
+		1)byte
+		2)int
+		3)bool
+		4)char(ingresar el valor entre comillas)
+		5)String(ingresar el valor entre comillas)
+		6)float
 
 
-
-gdsgsdfgsdfgsfgsd
-
-
+		""")
+	dato = int(input("Ingrese el tipo de variable: "))
 
 
-""")
+	if dato == 1:
+		dat = "byte"
+
+	if dato == 2:
+		dat = "int"
+
+
+	if dato == 3:
+		dat = "bool"
+
+
+	if dato == 4:
+		dat = "char"
+
+
+	if dato == 5:
+		dat = "String"
+
+
+	if dato == 6:
+		dat = "float"
+
+	val = str(input("nombre de la variable: "))
+	print("\n")
+	valor = str(input("valor de la variable (Si no quieres ningun valor dejalo en blanco): " ))
+	print("\n")
+	f.write(dat + " " + val + " " + "=" + " " +valor + ";" + "\n")
 
 
 
